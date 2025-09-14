98fd380b4afbe48cd22f35ce88ccbce9e7dca061
Fanarier <113526168+Fanarier@users.noreply.github.com>
2025-09-14 21:37:26 +0200

    main.js
    add Vuetify

--- vue-todolist/src/main.js
+++ vue-todolist/src/main.js
@@ -1,8 +1,24 @@
+// src/main.js
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // иконки
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// мои стили 
 import './style.css'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(vuetify)
 app.mount('#app')
